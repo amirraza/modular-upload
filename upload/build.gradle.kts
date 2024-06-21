a7d7233989ca89651438ddbbbc7f8ae14f115384
AmirRaza <amir.raza@purevpn.com>
2024-06-21 16:45:27 +0500

    Add maven local publishing

--- upload/build.gradle.kts
+++ upload/build.gradle.kts
@@ -1,14 +1,20 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("maven-publish")
 }
+
+kotlin {
+    jvmToolchain(17)
+}
+
 
 android {
     namespace = "com.example.upload"
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 27
+        minSdk = 28
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -23,16 +29,11 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
+
+    implementation(libs.modular.common)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -40,4 +41,18 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.example.modular"
+                artifactId = "upload"
+                version = libs.versions.upload.version.get()
+            }
+        }
+    }
 }
